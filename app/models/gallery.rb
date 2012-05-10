cfcc224294e209f0aa87000ef4f0ed673cc66959
ming <nicholaskhuu@lycos.com>
2012-05-10 17:45:24 -0400

    added attr acces

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ActiveRecord::Base
 
-  attr_accessible :image, :description, :featured, :lodging
+  attr_accessible :image, :description, :featured, :lodging, :name
   
   mount_uploader :image, GalleryUploader
 
