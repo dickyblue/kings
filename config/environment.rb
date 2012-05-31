f7365462e3319d13fa1cf2549b3637518912e222
ming <nicholaskhuu@lycos.com>
2012-05-31 18:17:00 -0400

    added mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 NickMing::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+:user_name => "minghuang",
+:password => "chicken128",
+:domain => "thekhuus.com",
+:address => "smtp.sendgrid.net",
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
+}
