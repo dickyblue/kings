a98113809ab72acd2cc87f7c14960bb809718ff1
ming <nicholaskhuu@lycos.com>
2012-06-01 11:04:30 -0400

    finished adding email

--- app/mailers/update_mailer.rb
+++ app/mailers/update_mailer.rb
@@ -2,10 +2,10 @@ class UpdateMailer < ActionMailer::Base
   default from: "nicholas.khuu@gmail.com"
   
   def update_email(user)
-    @user = user
     @url = "http://www.thekhuus.com"
     @weekend = "http://www.thekhuus.com/weekend_schedule"
-    mail(:to => user.email, :subject => "Nick and Ming updated their website")
+    @user = user
+    mail(:to => user.email, :subject => "Nick and Ming updated their website")   
   end
   
 end
