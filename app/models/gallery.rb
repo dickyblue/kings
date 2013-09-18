cacb25280a4403f0ba19a39fe5826516c6baf9c0
ming <nicholaskhuu@lycos.com>
2013-09-18 19:13:16 -0400

    added the ability to edit wedding and wedding cruise

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ActiveRecord::Base
   
-  attr_accessible :image, :description, :featured, :lodging, :name, :engagement, :friend_upload, :user_id, :friend_uploader
+  attr_accessible :image, :description, :featured, :lodging, :name, :engagement, :friend_upload, :user_id, :friend_uploader, :wedding, :wedding_cruise
   
   has_many :comments
   accepts_nested_attributes_for :comments
