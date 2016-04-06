5f22e1060f71da1a965dcc420cbeff5c72168836
Nick Khuu <nicholaskhuu@lycos.com>
2016-04-06 10:15:10 -0400

    git

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# Kings::Application.config.secret_token = 'c617ee568b9c89a0b02b1e5d2b2f05856c6f80188466f77b368e753f4c5313e8a7167a058b55a6f24c550c73f5925996c30cffdea20662f2e3ed3e3f275cccc0'
-Kings::Application.config.secret_key_base = ENV['SECRET_TOKEN']
